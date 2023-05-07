c0bd8d0e049a9d3f65abf1d369f702b0ed4bda4f
daniil <daniiltrotsenko2002@gmail.com>
2023-05-07 17:05:18 +0300

    Добавлен класс Ареа

--- Models.py
+++ Models.py
@@ -8,10 +8,13 @@ class Sprite:
         self.rect = pygame.Rect(x, y, 10, 10)
         self.image = pygame.image.load(image_name)
 
+    def draw(self, scr):
+        scr.blit(self.image, (self.rect.x, self.rect.y))
+
 
 class Ork(Sprite):
     def __init__(self, x, y,):
-        super().__init__(x, y, "ork_image.jpg")
+        super().__init__(x, y, "ork_image.png")
 
 
 class Ball(Sprite):
--- main.py
+++ main.py
@@ -7,6 +7,8 @@ scr = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 GameOver = False
 
+ork = Models.Ork(0, 0)
+ork.draw(scr)
 while not GameOver:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
